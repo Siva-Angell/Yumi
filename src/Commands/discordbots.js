248e88837a9c9d9fb768fb9682557b28218e61b1
August <AnAuguseh@users.noreply.github.com>
2017-07-02 10:05:35 -0700

    owner_only.dbots | Done

---
+++ src/Commands/discordbots.js
@@ -0,0 +1,14 @@
+var snekfetch = require('snekfetch');
+exports.run = function(ayane, msg) {
+	if (msg.author.id != "280158289667555328") return msg.reply("I'm not doing my stats because your not my owner!")
+msg.channel.send("Trying to post server stats!").then(mes => {
+	  setTimeout(() => {
+  snekfetch.post(`https://bots.discord.pw/api/bots/317145148901556234/stats`)
+  .set(`Authorization`, `TOKEN`)
+  .send({
+    server_count: ayane.guilds.size
+  })
+  .then(mes.edit(`Succesfully updated all guilds on bots.discord.pw`))
+}, 500)
+});
+}
