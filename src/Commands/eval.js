32558af5dd533d6d6d84a72fd0bebf07ba900015
August <AnAuguseh@users.noreply.github.com>
2017-07-02 10:06:43 -0700

    owner_only.eval | Done

---
+++ src/Commands/eval.js
@@ -0,0 +1,21 @@
+exports.run = function(ayane, msg, args) {
+  if(msg.author.id != '280158289667555328') return msg.reply("Eval is only for Owner Only! Sorry!")
+  try {
+    const code = args.join(" ");
+    let evaled = eval(code);
+
+    if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+
+    msg.channel.send(clean(evaled), {code:"js"});
+  } catch (err) {
+   msg.channel.send(`\`ERROR\` \`\`\`js\n${clean(err)}\n\`\`\``);
+  }
+}
+
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
