8b83f9352f7a613cbcee2a03d69b3d0d0e89506c
August <AnAuguseh@users.noreply.github.com>
2017-07-02 09:58:12 -0700

    fun.anime | Done

---
+++ src/Commands/anime.js
@@ -0,0 +1,39 @@
+const Kitsu                                 = require('kitsu.js');
+const kitsu                                 = new Kitsu();
+exports.run = (client, msg, params) => {
+    if (params.length < 1) return msg.reply('You must add a word to search for');
+    msg.channel.send(":ok_hand: Fetching anime from Kitsu!").then(message => {
+    kitsu.searchAnime(params.join(" "))
+      .then(result => {
+          const filter = message => {
+            if(message.content === "1" || message.content === "2" || message.content === "3" || message.content === "4" || message.content === "5") {
+              return true
+            } else {
+              return false
+            }
+          }
+          message.edit(`Found some animes! Type 1-5 on the text channel! (Cancels at 60 seconds!)\n1: ${result[0].titles.english}/${result[0].titles.japanese}\n2: ${result[1].titles.english}/${result[1].titles.japanese}\n3: ${result[2].titles.english}/${result[2].titles.japanese}\n4: ${result[3].titles.english}/${result[3].titles.japanese}\n5: ${result[4].titles.english}/${result[4].titles.japanese}`)
+          msg.channel.awaitMessages(filter, {
+              "max": 20,
+              "maxMatches": 1,
+              "time": 60000,
+              "errors": ['time']
+              }
+            ).then(message => {
+                if (message.size === 0) return
+                const number = Number(message.first().content) - 1
+				msg.channel.send(`Anime Search Query!\n\n**Titles:**\n**Japanese**: ${result[number].titles.japanese}\n**English**: ${result[number].titles.english}\n\n**Show Type**: ${result[number].showType}\n\n**Popularity Rank**: ${result[number].popularityRank}\n\n**Synopsis**: ${result[number].synopsis}`)
+              }
+            )
+            .catch(() => msg.reply("Command Canceled due to timer!"))
+
+        }
+      )
+      .catch(() => {
+          message.edit("Did you spell the anime right? I couldn't get your search query!")
+          msg.react("❓")
+          }
+        );
+      }
+    )
+}
