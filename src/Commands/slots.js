25c35f9e3f61254e39df8cc2c17c2f08508d03b8
August <AnAuguseh@users.noreply.github.com>
2017-07-05 21:43:45 -0700

    mmmm lol

--- src/Commands/slots.js
+++ src/Commands/slots.js
@@ -1,5 +1,5 @@
 let slots = require('../Settings/slots.json');
 exports.run = function(ayane, msg) {
   msg.channel.send("**Slots**! Let's see the result")
-  msg.channel.send(slot[Math.floor(Math.random() * 3) +1]);
+  msg.channel.send(slots[Math.floor(Math.random() * 3) +1]);
 };
