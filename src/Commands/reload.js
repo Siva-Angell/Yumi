b6a0c530bd51efc5afb24b7c00426727f9af554b
Desiree <diallusions@gmail.com>
2017-07-07 14:54:22 -0400

    hinata -> yumi

--- src/Commands/reload.js
+++ src/Commands/reload.js
@@ -1,5 +1,5 @@
 exports.run = (ayane, msg, args) => {
-  if(!args || args.size < 1) return msg.channel.send("Please specify a command. (EXAMPLE: `h;reload help.js`)");
+  if(!args || args.size < 1) return msg.channel.send("Please specify a command. (EXAMPLE: `y/reload help.js`)");
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   msg.channel.send(`The command ${args[0]} has been reloaded`);
 };
