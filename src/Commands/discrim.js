37f3290be334c309a43610a71034677f340b9e15
Desiree <diallusions@gmail.com>
2017-07-07 14:52:03 -0400

    "Ayane" that needed to be updated

--- src/Commands/discrim.js
+++ src/Commands/discrim.js
@@ -1,14 +1,14 @@
 var Discord = require('discord.js');
 exports.run = function(ayane, msg) {
    const discrim = msg.content.split(' ')[1]
-   if (!discrim) return msg.reply("Can't find that discriminator!")
+   if (!discrim) return msg.reply("I can't find that discriminator!")
    if (typeof discrim !== 'integer')
-   if (discrim.size < 4) return msg.reply("Discrims are 4 numbers!")
-   if (discrim.size > 4) return msg.reply("Discrims are 4 numbers!")
+   if (discrim.size < 4) return msg.reply("Discriminators are 4 numbers!")
+   if (discrim.size > 4) return msg.reply("Discriminators are 4 numbers!")
 	 let members = ayane.users.filter(c=>c.discriminator===discrim).map(c=>c.username).join('\n')
 	 if (!members) return msg.reply("No members have that discriminator!")
    let embed = new Discord.RichEmbed()
-     .setTitle("Ayane | Discriminator")
+     .setTitle("Yumi | Discriminator")
 	 .setDescription("fun.discrim")
       .addField("Members:", members)
 	  msg.channel.send({embed: embed})
