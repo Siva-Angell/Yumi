39ac91a6de567c16de09f8bf42fe5bfd6c352b8b
August <AnAuguseh@users.noreply.github.com>
2017-07-09 10:49:26 -0700

    Slots(tm)

--- src/Commands/slots.js
+++ src/Commands/slots.js
@@ -1,4 +1,9 @@
-let slots = require('../Settings/slots.json');
+const slots = [
+   ":rosette:**|**:rosette:**|**:rosette: - You won!",
+   ":rosette:**|**:rosette:**|**:shit: - You lost, Just 1 shit?",
+   ":rosette:**|**:shit:**|**:shit: - You lost, 2 shits? xD",
+   ":shit:**|**:shit:**|**:shit: - Damn, You lost!"
+  ];
 exports.run = function(ayane, msg) {
   msg.channel.send("**Slots**! Let's see the result")
   msg.channel.send(slots[Math.floor(Math.random() * 3) +1]);
