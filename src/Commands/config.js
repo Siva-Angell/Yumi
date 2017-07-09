e027b867acdde6010035b870973049b8a42df68d
Desiree <diallusions@gmail.com>
2017-07-09 13:07:09 -0400

    The bold shit looked weird, like wtf

--- src/Commands/config.js
+++ src/Commands/config.js
@@ -7,16 +7,16 @@ exports.run = function(ayane, msg, args) {
 	  sql.run('INSERT INTO guilds (guildID, prefix) VALUES (?, ?)', [msg.guild.id, 'a;'],
    };
    let embed = new Discord.RichEmbed()
-     .setAuthor(`${ayane.user.username} Config! (finally)`, ayane.user.displayAvatarURL)
+     .setAuthor(`${ayane.user.username} Configuration! (finally)`, ayane.user.displayAvatarURL)
 	 .setDescription("Heyo! :wave: You can change any configuration, `disableAnnouncements` is coming soon! This is the prototype of the config; So this is not the	'final' command")
 	 .addField("`guild.setPrefix`", guild.prefix, true)
 	 msg.channel.send({embed})
 	 
 	 if(msg.content.toLowerCase().includes(` prefix`)) {
 	   let setprefix = args.slice(1).join(' ');
-	   if (!setprefix) return msg.reply("You must include a prefix!")
+	   if (!setprefix) return msg.reply("You must specify a prefix to change to!")
 	sql.run(`UPDATE guilds SET prefix = '${setprefix}' WHERE guildID = ${message.guild.id}`)
-	  msg.channel.send(`**Successfully** changed your prefix to \`${setprefix}\``)
+	  msg.channel.send(`**Your prefix was successfully changed to:** \`${setprefix}\``)
 	};
   } else {
    msg.channel.send({embed})
