357e3b86d7b2c48fee79b798124a67fcd8e63340
August <AnAuguseh@users.noreply.github.com>
2017-07-09 19:56:00 -0700

    Update Yumi.js

--- Yumi.js
+++ Yumi.js
@@ -1,26 +1,56 @@
-var YumiCore = require('discord.js');
-var yumi = new YumiCore.Client();
+const YumiCore = require('discord.js');
+const settings = require('./settings.json');
+const yumi = new YumiCore.Client();
 
 yumi.on('ready', () => {
-    ayane.user.setGame(`y/help | 404'ed it! | [${yumi.guilds.size} / ${yumi.users.size}]`)
-    console.log("Connected.")
+    yumi.user.setGame(`y/help  | Shard: ${yumi.shard.id} | [${yumi.guilds.size} / ${yumi.users.size}]`)
+    console.log("Ready mother fucker")
 });
 
-var prefix = "y/";
 yumi.on("message", msg => {
   if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
+  if (!msg.content.startsWith(settings.prefix)) return;
 
   const args = msg.content.split(" ");
-  const command = args.shift().slice(prefix.length)
+  const command = args.shift().slice(settings.prefix.length)
 
   try {
     let commandFile = require(`./src/commands/${command}.js`)
-    commandFile.run(ayane, msg, args)
+    commandFile.run(yumi, msg, args)
   } catch (err) {
     console.error(err)
   }
 });
+
+yumi.on('guildCreate', guild => {
+  postServerStats();
+    yumi.channels.get('333638048283623435').send(`<:robotAdd:326804186262142977> **Joined** a guild!\n**Guild Name**: ${guild.name}!\n**Guild Owner**: ${guild.owner} (${guild.owner.id})`)
+});
+
+yumi.on('guildDelete', guild => {
+  postServerStats();
+    yumi.channels.get('333638048283623435').send(`<:robotRemove:326804186932969472> **Left** a guild!\n**Guild Name**: ${guild.name}\n**Guild Owner**: ${guild.owner} (${guild.owner.id})`)
+});
+
+let snekfetch = require('snekfetch');
+function postServerStats() {
+  // post to discordbots.org!
+  snekfetch.post(`https://discordbots.org/api/bots/317145148901556234/stats`)
+    .set("Authorization", `dBotsToken`)
+    .send({
+      server_count: yumi.guilds.size
+    })
+    .then(console.log("Posted discordbots.org count."))
+
+    // post to bots.discord.pw
+    snekfetch.post(`https://bots.discord.pw/api/bots/317145148901556234/stats`)
+    .set("Authorization", `TOKEN`)
+    .send({
+      server_count: yumi.guilds.size
+    })
+    .then(console.log("Posted bots.discord.pw count."))
+}
+
 
 yumi.on('error', (e) => console.error(e));
 yumi.on("warn", (e) => console.warn(e));
