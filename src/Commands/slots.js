f814dd67754a194f1f558e55733cfae4d1c98e9e
August <AnAuguseh@users.noreply.github.com>
2017-07-13 11:14:47 -0700

    let's see if a variable can use snekfetch sorry about the long desc
    but I need to do this xDDDD

--- src/Commands/slots.js
+++ src/Commands/slots.js
@@ -8,6 +8,13 @@ const slots = [
    ":rosette:**|**:shit:**|**:shit:\nYou lost, 2 shits? xD",
    ":shit:**|**:shit:**|**:shit:\nDamn, You lost!"
   ];
+const snekfetch = require('snekfetch'); //npm install snekfetch (Come with Discord.js <3)
 exports.run = function(ayane, msg) {
+ let updoots = snekfetch.get("https://discordbots.org/api/bots/${id}/votes")
+     .set(`Authorization`, `TOKEN`)
+     .then(r => {
+        r.body.length
+     });
+  if (!updoots) return msg.reply(`You must upvote on discordbots.org to use this command! `Developers` have access to it lol.\n**Upvotes**: ${updoots}`);
   msg.channel.send("**Slots**! Let's see the result\n" + slots[Math.floor(Math.random() * 3) +1]))
 };
