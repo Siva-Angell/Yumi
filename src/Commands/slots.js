f2726809cbdfae29e8df512684417bc3b7cd3862
Michael Cao <mike.x.cao@gmail.com>
2017-07-22 12:56:05 -0400

    Update slots.js

--- src/Commands/slots.js
+++ src/Commands/slots.js
@@ -1,20 +1,21 @@
 const slots = [
-   ":trophy:**|**:trophy:**|**:trophy:\nDamn! You win, You must be lucky",
-   ":shit:**|**:trophy:**|**:trophy\nNice, 2!",
-   ":shit:**|**:shit:**|**:trophy\nLost? xD",
-   ":shit:**|**:shit**|**:shit:\nDAMN You didn't win trophies!",
-   ":rosette:**|**:rosette:**|**:rosette:\nYou won!",
-   ":rosette:**|**:rosette:**|**:shit:\nYou lost, Just 1 shit?",
-   ":rosette:**|**:shit:**|**:shit:\nYou lost, 2 shits? xD",
-   ":shit:**|**:shit:**|**:shit:\nDamn, You lost!"
-  ];
+	":trophy:**|**:trophy:**|**:trophy:\nDamn! You win, You must be lucky",
+	":shit:**|**:trophy:**|**:trophy\nNice, 2!",
+	":shit:**|**:shit:**|**:trophy\nLost? xD",
+	":shit:**|**:shit**|**:shit:\nDAMN You didn't win trophies!",
+	":rosette:**|**:rosette:**|**:rosette:\nYou won!",
+	":rosette:**|**:rosette:**|**:shit:\nYou lost, Just 1 shit?",
+	":rosette:**|**:shit:**|**:shit:\nYou lost, 2 shits? xD",
+	":shit:**|**:shit:**|**:shit:\nDamn, You lost!"
+];
+const config = require('../Settings/config.json');
 const snekfetch = require('snekfetch'); //npm install snekfetch (Come with Discord.js <3)
-exports.run = function(ayane, msg) {
- let updoots = snekfetch.get("https://discordbots.org/api/bots/${id}/votes")
-     .set(`Authorization`, `TOKEN`)
-     .then(r => {
-        r.body.length
-     });
-  if (!updoots) return msg.reply(`You must upvote on discordbots.org to use this command! `Developers` have access to it lol.\n**Upvotes**: ${updoots}`);
-  msg.channel.send("**Slots**! Let's see the result\n" + slots[Math.floor(Math.random() * 3) +1]))
+exports.run = function(yumi, msg) {
+	let updoots = snekfetch.get(`https://discordbots.org/api/bots/${yumi.user.id}/votes`)
+		.set(`Authorization`, config.oliyBots)
+		.then(r => {
+				if (!r.body.includes(msg.author.id)) returnmsg.reply(`You must upvote on discordbots.org to use this command!\n**Total Upvotes**: ${r.body.length}`);
+				msg.channel.send("**Slots**! Let's see the result\n" + slots[Math.floor(Math.random() * 3) + 1]))
+		});
+
 };
